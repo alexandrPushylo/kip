e61e74a4b40763339a1e60f536d446312c4d8593
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2023-02-10 01:08:36 +0300

    fix js

--- public_html/static/admin/js/scripts/main.js
+++ public_html/static/admin/js/scripts/main.js
@@ -1,12 +1,3 @@
 $('.app_description').each(function () {
     this.style.height = ""+(this.scrollHeight)+"px";
 });
-
-$(".btn_driver_panel").click(function () {
-    if($('.driver_panel').is(':hidden')){
-        $('.driver_panel').prop('hidden',false)
-    }else {
-        $('.driver_panel').prop('hidden',true)
-    }
-    return false
-})
