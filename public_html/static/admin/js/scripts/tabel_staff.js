556d03fa879870d506d3fd28cba3035003c1c091
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2023-02-12 16:30:52 +0300

    add ajax

--- public_html/static/admin/js/scripts/tabel_staff.js
+++ public_html/static/admin/js/scripts/tabel_staff.js
@@ -1,4 +1,6 @@
 const count_row = $('.user_row').length;
+const csrf = $('input[name="csrfmiddlewaretoken"]').val();
+const pathname = window.location.pathname
 
 for (let i=1;i<=count_row;i++) {
 
@@ -8,7 +10,21 @@ for (let i=1;i<=count_row;i++) {
         } else {
 	        $('#inp_'+i).prop('checked', true);
         }
-
+        sent(i)
 
     })
+}
+function sent(i) {
+        let id_drv = $('#drv_id_'+i).val();
+        let status = $('#inp_'+i).is(':checked');
+        $.ajax({
+        type: 'POST',
+        mode: 'same-origin',
+        url: pathname,
+        data:{
+            csrfmiddlewaretoken: csrf,
+                id_drv: id_drv,
+                status: status
+            }
+        })
 }
